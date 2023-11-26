ea441c28767aff03213dff761fd769009fb9bf97
BuildTools <unconfigured@null.spigotmc.org>
2023-11-26 17:08:43 -0500

    still need to figure out how to configure shadowJar for fabric

--- neon-mod/fabric-mod/src/client/java/io/github/pulsebeat02/neon/Neon.java
+++ neon-mod/fabric-mod/src/client/java/io/github/pulsebeat02/neon/Neon.java
@@ -5,6 +5,7 @@ import io.github.pulsebeat02.neon.listener.PluginMessagingResponse;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class Neon implements ClientModInitializer {
+
   @Override
   public void onInitializeClient() {
     new ServerJoinHandler();
